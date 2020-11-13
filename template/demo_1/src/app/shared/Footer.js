8f09ecb29dedcd2886551a029b72584d2f024a6e
bootstrapdash-team <git@bootstrapdash.com>
2020-11-13 11:45:34 +0530

    copyright text edit

--- template/demo_1/src/app/shared/Footer.js
+++ template/demo_1/src/app/shared/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
       <footer className="footer">
         <div className="d-sm-flex justify-content-center justify-content-sm-between py-2">
           <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank" rel="noopener noreferrer">bootstrapdash.com </a>2020</span>
-          <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://www.bootstrapdash.com/react-admin-templates/" target="_blank" rel="noopener noreferrer"> react admin </a> templates from BootstrapDash.  </span>
+          <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/react-admin-templates/" target="_blank" rel="noopener noreferrer"> react admin </a> templates from BootstrapDash.com.  </span>
         </div>
       </footer> 
     );
